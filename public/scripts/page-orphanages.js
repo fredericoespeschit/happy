bf03848759d9f53ab983fab349c40f351679781f
Frederico Rodrigues Dell A Espeschit <fespeschitt@gmail.com>
2020-10-14 23:53:31 -0300

    Mapa

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,21 @@
+const map = L.map('mapid').setView([-19.917375,-43.9417858], 16);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//Create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+const popup = L.popup ({
+    closeButton: false,
+    className: "map-popup",
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>');
+
+L.marker([-19.917375,-43.9417858], { icon: icon }).addTo(map)
+    .bindPopup(popup)
